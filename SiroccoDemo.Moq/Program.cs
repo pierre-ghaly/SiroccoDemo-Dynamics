da36795227c1d6b3fd59ba51f197f04de06f2da2
pierre-ghaly <pierre.ghaly@gmail.com>
2025-06-17 21:13:50 +0300

    Added Plugins Moq Project

---
+++ SiroccoDemo.Moq/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using SiroccoDemo.Moq.Activities;
+using System;
+using System.Configuration;
+
+namespace SiroccoDemo.Moq
+{
+    class Program
+    {
+        public static IOrganizationService OrganizationService { get; private set; }
+
+        static void Main(string[] args)
+        {
+            var service = GetAccessToCRM();
+            CreateAccountWithContactsAndNotesMoq.ExecuteMoq(service);
+        }
+
+        public static CrmServiceClient GetAccessToCRM()
+        {
+            try
+            {
+
+                CrmServiceClient crmSvc = new CrmServiceClient(ConfigurationManager.ConnectionStrings["Moq"].ConnectionString);
+
+                return crmSvc;
+            }
+            catch (Exception ex)
+            {
+
+                if (ex.InnerException != null)
+                {
+                }
+                throw;
+            }
+        }
+    }
+}
